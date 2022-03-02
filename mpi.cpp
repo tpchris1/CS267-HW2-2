0a3d6b6515d04563a3bcbe98afe22fbac69e33e2
tpchris1 <topchrischang@hotmail.com>
2022-03-01 16:28:14 -0800

    Update MPI with initialization

--- mpi.cpp
+++ mpi.cpp
@@ -1,12 +1,101 @@
 #include "common.h"
 #include <mpi.h>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 // Put any static global variables here that you will use throughout the simulation.
+typedef vector<particle_t*> bin_t;
 
-void init_simulation(particle_t* parts, int num_parts, double size, int rank, int num_procs) {
+// total 
+double size;
+bin_t* bins;
+int bin_row_count;
+int bin_count;
+double bin_size;
+
+// for each process
+int rows_per_proc;
+int proc_bin_count;
+int proc_rows_start;
+int proc_rows_end; 
+
+int inline get_row_id(int bin_id){
+    
+}
+
+int inline get_bin_id(particle_t& particle) {
+    int x, y;
+    x = particle.x / bin_size;
+    y = particle.y / bin_size;
+    if (x == bin_row_count) {
+        x--;
+    }
+    if (y == bin_row_count) {
+        y--;
+    }
+    return y * bin_row_count + x;
+}
+
+void reconstruct_bin(particle_t* parts, int num_parts) {
+    
+    for (int i = 0; i < bin_count; i++) {
+        bins[i].clear();
+    }
+
+    for (int i = 0; i < num_parts; i++) {
+        parts[i].ax = 0;
+    	parts[i].ay = 0;
+        int bin_id = get_bin_id(parts[i]);
+        bins[bin_id].push_back(&parts[i]);
+    }
+}
+
+void init_simulation(particle_t* parts, int num_parts, double size_, int rank, int num_procs) {
 	// You can use this space to initialize data objects that you may need
 	// This function will be called once before the algorithm begins
 	// Do not do any particle simulation here
+
+    // total
+    bin_row_count = size_ / cutoff;
+    bin_count = bin_row_count * bin_row_count;
+    bin_size = size_ / bin_row_count;
+    
+    rows_per_proc = bin_row_count / num_procs; 
+    
+    // with remainder
+    int remainder = bin_row_count % num_procs; 
+    if (remainder != 0 && rank < remainder){
+        rows_per_proc += 1;
+    }   
+    proc_bin_count = rows_per_proc * bin_row_count;
+    bins = new bin_t[proc_bin_count];
+    
+    if (remainder != 0 && rank < remainder){
+        proc_rows_start = rank * rows_per_proc;
+        proc_rows_end = proc_rows_start + rows_per_proc;
+    }
+    else{
+        int rows_before_me = remainder * (rows_per_proc + 1);
+        proc_rows_start = rows_before_me + (rank-remainder) * rows_per_proc;
+        proc_rows_end = proc_rows_start + rows_per_proc;
+    }   
+    
+    cout << "rank: " << rank << endl;
+    cout << "rows_per_proc: " << rows_per_proc << endl;
+    cout << "proc_rows_start: " << proc_rows_start << endl;
+    cout << "proc_rows_end: " << proc_rows_end << endl;
+    cout << "proc_bin_count: " << proc_bin_count << endl;
+    // if (rank == 0) {
+    //     cout << size << endl;
+    //     cout << bin_row_count << endl;
+    //     cout << bin_count << endl;
+    //     cout << bin_size << endl;
+    // }
+    cout << endl;
+
 }
 
 void simulate_one_step(particle_t* parts, int num_parts, double size, int rank, int num_procs) {
